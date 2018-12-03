12baa62038ae42f249a86e52b1cf48796d729739
Estimon <ploompuumartin@gmail.com>
2018-12-03 09:49:53 +0200

    456

---
+++ App1/App1/ImageAdapter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    
+    public class ImageAdapter : BaseAdapter
+    {
+        Context context;
+
+        public ImageAdapter(Context c)
+        {
+            context = c;
+        }
+
+        public override int Count
+        {
+            get { return thumbIds.Length; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageview;
+            if(convertView == null)
+            {
+                imageview = new ImageView(context);
+                imageview.LayoutParameters = new GridView.LayoutParams(85, 85);
+                imageview.SetScaleType(ImageView.ScaleType.CenterCrop);
+                imageview.SetPadding(8, 8, 8, 8);
+
+            }
+            else
+            {
+                imageview = (ImageView)convertView;
+            }
+
+            imageview.SetImageResource(thumbIds[position]);
+            return imageview;
+
+            
+        }
+        int[] thumbIds =
+        {
+        Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+        Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+        Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+        Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+        Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6
+        };
+    }
+
+}
